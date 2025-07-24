32091e11be99e8277dc9cc50f825da590ce5dbbc
Guillaume Louel <37544189+glouel@users.noreply.github.com>
2025-07-24 18:42:21 +0200

    Update code for Tahoe bugs

--- ScreenSaverMinimal/ScreenSaverMinimalView.swift
+++ ScreenSaverMinimal/ScreenSaverMinimalView.swift
@@ -13,20 +13,29 @@ class ScreenSaverMinimalView : ScreenSaverView {
     
     lazy var sheetController: ConfigureSheetController = ConfigureSheetController()
     var isPreviewBug: Bool = false
+    var originalIsPreview: Bool = false
     
     override init(frame: NSRect, isPreview: Bool) {
-        // Radar# FB7486243, legacyScreenSaver.appex always returns true, unlike what used
-        // to happen in previous macOS versions, see documentation here : https://developer.apple.com/documentation/screensaver/screensaverview/1512475-init$
 
-        var preview = true
-
-        // We can workaround that bug by looking at the size of the frame
-        // It's always 296.0 x 184.0 when running in preview mode
-        if frame.width > 400 && frame.height > 300 {
-            if isPreview {
-                isPreviewBug = true
+        var preview = isPreview
+        originalIsPreview = isPreview
+        
+        // isPreview is differently bugged depending on macOS versions. The common bug pre macOS 26 can be workarounded. The Tahoe+ bug has no workaround at this time.
+        if #available(macOS 26.0, *) {
+            preview = isPreview
+        } else {
+            // Radar# FB7486243, legacyScreenSaver.appex always returns true, unlike what used
+            // to happen in previous macOS versions, see documentation here : https://developer.apple.com/documentation/screensaver/screensaverview/1512475-init$
+            // This is only true pre-macOS Tahoe, that has the opposite bug, that we can't workaround this way!
+            preview = true
+            // We can workaround that bug by looking at the size of the frame
+            // It's always 296.0 x 184.0 when running in preview mode
+            if frame.width > 400 && frame.height > 300 {
+                if isPreview {
+                    isPreviewBug = true
+                }
+                preview = false
             }
-            preview = false
         }
         
         super.init(frame: frame, isPreview: preview)!
@@ -56,16 +65,46 @@ class ScreenSaverMinimalView : ScreenSaverView {
     
 
     override func draw(_ rect: NSRect) {
-        let bPath:NSBezierPath = NSBezierPath(rect: bounds)
+        // Fill entire rect with border color
         Preferences.canvasColor.nsColor.set()
-        bPath.fill()
-
-        // This is the debug view
-        let debugTextView = NSTextView(frame: bounds.insetBy(dx: 20, dy: 20))
-        debugTextView.font = .labelFont(ofSize: 30)
-        debugTextView.string += "Radar# FB7486243 (isPreview bug) : \(isPreviewBug) \n"
+        NSBezierPath(rect: bounds).fill()
         
-        self.addSubview(debugTextView)
+        // Clear the inner area
+        let borderWidth = bounds.width * 0.05
+        let innerRect = bounds.insetBy(dx: borderWidth, dy: borderWidth)
+        
+        // Use dark purple if in preview mode, black otherwise
+        let innerColor = originalIsPreview ? NSColor(red: 0.3, green: 0.1, blue: 0.4, alpha: 1.0) : NSColor.black
+        innerColor.set()
+        NSBezierPath(rect: innerRect).fill()
+
+        // Draw preview status text
+        let previewText = "Preview: \(originalIsPreview ? "YES" : "NO")"
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: NSFont.boldSystemFont(ofSize: 24),
+            .foregroundColor: NSColor.white
+        ]
+        
+        let textSize = previewText.size(withAttributes: attributes)
+        let xPosition = bounds.width * 0.1
+        let yPosition = bounds.height - borderWidth - textSize.height - 20
+        
+        previewText.draw(at: NSPoint(x: xPosition, y: yPosition), withAttributes: attributes)
+        
+        // Only show Radar# FB7486243 on macOS versions before 26.0
+        if #unavailable(macOS 26.0) {
+            // Draw debug text in bottom left
+            let debugText = "Radar# FB7486243 (isPreview bug): \(isPreviewBug)"
+            let debugAttributes: [NSAttributedString.Key: Any] = [
+                .font: NSFont.boldSystemFont(ofSize: 24),
+                .foregroundColor: NSColor.white
+            ]
+            
+            let debugXPosition = bounds.width * 0.1
+            let debugYPosition = borderWidth + 20
+            
+            debugText.draw(at: NSPoint(x: debugXPosition, y: debugYPosition), withAttributes: debugAttributes)
+        }
 
     }
     
